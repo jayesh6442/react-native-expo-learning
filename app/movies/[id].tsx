fbbdfe689fabec3625848c17dc6325bbcaf780bf
jayesh <jayeshsarvaiya9913@gmail.com>
2026-01-19 14:35:15 +0530

    END TO END ROUTS FOR MOVIE SHOW PLATFORM

---
+++ app/movies/[id].tsx
@@ -0,0 +1,12 @@
+import {  Text, View } from 'react-native'
+import React from 'react'
+
+const MovieDetail = () => {
+  return (
+    <View>
+      <Text>MovieDetail</Text>
+    </View>
+  )
+}
+
+export default MovieDetail
